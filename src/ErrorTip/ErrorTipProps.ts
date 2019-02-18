41d6fe0f720d22c9987066b49d31d1b12d3827b6
MAKARD <human.getEmail@gmail.com>
2019-02-18 10:34:38 +0200

    Write tests

--- src/ErrorTip/ErrorTipProps.ts
+++ src/ErrorTip/ErrorTipProps.ts
@@ -3,9 +3,9 @@ import * as PropTypes from "prop-types";
 import { FormGroupContextInterface } from "react-formawesome-core";
 
 export interface ErrorTipProps extends React.HTMLProps<HTMLSpanElement> {
-    children: React.ReactNode | ((error: FormGroupContextInterface["error"]) => React.ReactNode);
+    children?: (error: FormGroupContextInterface["error"]) => React.ReactNode;
 }
 
 export const ErrorTipPropTypes: Partial<PropTypes.ValidationMap<ErrorTipProps>> = {
-    children: PropTypes.oneOfType([PropTypes.node, PropTypes.func]).isRequired
+    children: PropTypes.func
 };
