f66bb861ae1fc8623caceea224795a5224e98287
MAKARD <human.getEmail@gmail.com>
2019-02-20 14:31:41 +0200

    Write tests

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -4,84 +4,82 @@ import { FormGroupContext, FormGroupContextInterface as Context } from "react-fo
 import { CheckboxProps, CheckboxPropTypes, CheckboxDefaultProps } from "./CheckboxProps";
 
 export class Checkbox extends React.Component<CheckboxProps> {
-	public static readonly propTypes = CheckboxPropTypes;
-	public static readonly defaultProps = CheckboxDefaultProps;
+    public static readonly propTypes = CheckboxPropTypes;
+    public static readonly defaultProps = CheckboxDefaultProps;
 
-	protected unregister?: Context["unregisterElement"];
+    protected unregister?: Context["unregisterElement"];
 
-	public componentWillUnmount(): void {
-		this.unregister && this.unregister();
-	}
+    public componentWillUnmount(): void {
+        this.unregister && this.unregister();
+    }
 
-	public render(): React.ReactNode {
-		return (
-			<FormGroupContext.Consumer>
-				{this.renderChildren}
-			</FormGroupContext.Consumer>
-		);
-	}
+    public render(): React.ReactNode {
+        return (
+            <FormGroupContext.Consumer>
+                {this.renderChildren}
+            </FormGroupContext.Consumer>
+        );
+    }
 
-	protected renderChildren = (context: Context): React.ReactNode => {
-		const {
-			errorClassName,
-			activeClassName,
-			nativeRef,
-			className,
-			onChange,
-			checked,
-			values,
-			type,
-			ref,
-			...checkboxProps
-		} = this.props;
+    protected renderChildren = (context: Context): React.ReactNode => {
+        const {
+            errorClassName,
+            activeClassName,
+            nativeRef,
+            className,
+            onChange,
+            checked,
+            values,
+            value,
+            type,
+            ref,
+            ...checkboxProps
+        } = this.props;
 
-		const [activeValue] = values;
+        const [activeValue] = values;
 
-		return (
-			<input
-				{...checkboxProps}
+        return (
+            <input
+                {...checkboxProps}
 
-				onChange={this.getHandleChange(context)}
+                onChange={this.getHandleChange(context)}
 
-				checked={activeValue === context.value}
-				className={this.getClassName(context)}
-				ref={this.registerElement(context)}
-				type="checkbox"
-			/>
-		);
-	};
+                checked={activeValue === context.value}
+                className={this.getClassName(context)}
+                ref={this.registerElement(context)}
+                value={context.value}
+                type="checkbox"
+            />
+        );
+    };
 
-	protected registerElement = (context: Context) => (instance?: HTMLInputElement): void => {
-		this.props.nativeRef && this.props.nativeRef(instance);
+    protected registerElement = (context: Context) => (instance?: HTMLInputElement): void => {
+        this.props.nativeRef && this.props.nativeRef(instance);
 
-		if (!instance || this.unregister) {
-			return;
-		}
+        if (!instance || this.unregister) {
+            return;
+        }
 
-		context.registerElement(instance);
+        context.registerElement(instance);
 
-		this.unregister = context.unregisterElement;
-	}
+        this.unregister = context.unregisterElement;
+    }
 
-	protected getHandleChange = (context: Context) => (event: React.ChangeEvent<HTMLInputElement>): void => {
-		this.props.onChange && this.props.onChange(event);
+    protected getHandleChange = (context: Context) => (event: React.ChangeEvent<HTMLInputElement>): void => {
+        this.props.onChange && this.props.onChange(event);
 
-		const [activeValue, disabledValue] = this.props.values;
+        const [activeValue, disabledValue] = this.props.values;
 
-		if (context.value === activeValue) {
-			context.onChange(disabledValue);
-		} else if (context.value === disabledValue) {
-			context.onChange(activeValue);
-		}
-	}
+        context.onChange(context.value === activeValue ? disabledValue : activeValue);
+    }
 
-	protected getClassName = (context: Context): string => {
-		const [activeValue] = this.props.values;
+    protected getClassName = (context: Context): string => {
+        const [activeValue] = this.props.values;
 
-		return [
-			this.props.className,
-			context.error && this.props.errorClassName,
-			context.value === activeValue && this.props.activeClassName
-		].filter((className) => className).join(" ").trim() || undefined;
-	}
+        return [
+            this.props.className,
+            context.error && this.props.errorClassName,
+            context.value === activeValue && this.props.activeClassName
+        ].filter((className) => className).join(" ").trim() || undefined;
+    }
 }
--- src/Checkbox/CheckboxProps.ts
+++ src/Checkbox/CheckboxProps.ts
@@ -2,24 +2,24 @@ import * as React from "react";
 import * as PropTypes from "prop-types";
 
 export interface CheckboxProps extends React.HTMLProps<HTMLInputElement> {
-	values: [string, string];
+    values: [string, string];
 
-	errorClassName?: string;
-	activeClassName?: string;
+    errorClassName?: string;
+    activeClassName?: string;
 
-	nativeRef?: (instance?: HTMLInputElement) => void;
+    nativeRef?: (instance?: HTMLInputElement) => void;
 }
 
 export const CheckboxPropTypes: Partial<PropTypes.ValidationMap<CheckboxProps>> = {
-	values: PropTypes.arrayOf(PropTypes.string).isRequired as PropTypes.Requireable<[string, string]>,
+    values: PropTypes.arrayOf(PropTypes.string).isRequired as PropTypes.Requireable<[string, string]>,
 
-	errorClassName: PropTypes.string,
-	activeClassName: PropTypes.string,
+    errorClassName: PropTypes.string,
+    activeClassName: PropTypes.string,
 
-	nativeRef: PropTypes.func
+    nativeRef: PropTypes.func
 };
 
 export const CheckboxDefaultProps: Partial<CheckboxProps> = {
-	errorClassName: "has-error",
-	activeClassName: "is-active"
+    errorClassName: "has-error",
+    activeClassName: "is-active"
 };
